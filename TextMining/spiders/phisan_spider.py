208af2ad695681eac717a12bd703fa32d0439b07
Phisan Shukkhi <phisanshukkhi@Phisans-Mac-mini.local>
2015-10-16 23:45:10 +0700

    modified:   TextMining/spiders/items.csv 	modified:   TextMining/spiders/phisan_spider.py 	modified:   TextMining/spiders/phisan_spider.pyc

--- TextMining/spiders/phisan_spider.py
+++ TextMining/spiders/phisan_spider.py
@@ -13,7 +13,6 @@ class TextMiningSpider(scrapy.Spider):
     ]
 
     start_urls = [
-        "http://phisan.sskru.ac.th",
         "http://health.kapook.com/herb",
         "http://frynn.com/herb",
         "http://frynn.com/%e0%b8%a3%e0%b8%b2%e0%b8%a2%e0%b8%8a%e0%b8%b7%e0%b9%88%e0%b8%ad%e0%b8%aa%e0%b8%a1%e0%b8%b8%e0%b8%99%e0%b9%84%e0%b8%9e%e0%b8%a3",
@@ -32,18 +31,30 @@ class TextMiningSpider(scrapy.Spider):
         for selector in selectors:
             for sel in response.xpath(selector):
                 item = TextminingItem()
-                item['title'] = sel.xpath('a/text()').extract()
-                item['link'] = sel.xpath('a/@href').extract()
+                titles = sel.xpath('a/text()').extract()
+                links = sel.xpath('a/@href').extract()
 
-                if len(item['link']) != 0:
-                    r = urlparse(item['link'][0])
-                    item['netloc'] = r[1]
-
-                    if item['netloc'] == '':
-                        item['link'] = urljoin(response.url, item['link'][0])
-                        r = urlparse(item['link'])
+                # for multiple links
+                if len(links) != 0:
+                    index = 0
+                    for link in links:
+                        item['link'] = link
+                        r = urlparse(link)
                         item['netloc'] = r[1]
-                    yield item
+
+                        if len(titles) != 0:
+                            item['title'] = titles[index]
+                        else:
+                            item['title'] = titles
+
+                        # for relative path link
+                        if item['netloc'] == '':
+                            item['link'] = urljoin(response.url, link)
+                            r = urlparse(item['link'])
+                            item['netloc'] = r[1]
+
+                        index += 1
+                        yield item
 
 
 
